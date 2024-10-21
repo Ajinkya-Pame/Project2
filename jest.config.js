9e93beca7ee676ec4b0e382c545caeebf3a79f89
Ajinkya Pame <121514710+Ajinkya-Pame@users.noreply.github.com>
2024-10-22 01:06:21 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  testMatch: [
+    "**/tests/**/*.[jt]s?(x)", // Adjust this line to include the tests folder
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  // You can add other configurations if needed
+};
